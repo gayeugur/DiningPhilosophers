dbb89b39059955bd93e032bcd6a82a45df5c5079
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2022-01-03 18:21:36 +0300

    Update lokanta.c

--- lokanta.c
+++ lokanta.c
@@ -1,19 +1,14 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
 #include <time.h>
 #include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #define NUMBER_OF_PHILOSOPHERS 8
 #define NUMBER_OF_TABLE 10
 
-double priceTableOpen=99.90;
-double priceTableReorganize=19.90;
-double priceRice=20;
-double riceTableAmount=2000;
-double eatingRiceQuantity=100;
-double price;
 
 struct Philosopher
 {
@@ -22,7 +17,7 @@ struct Philosopher
 	int rightForkIndex;
 	int eatenTimes;	
 	pthread_t thread_id;
-} philosophers;
+};
 
 struct Fork
 {
@@ -30,107 +25,160 @@ struct Fork
 	sem_t mutex;
 };
 
+struct Fork* forks;
 sem_t global_mutex;
 int NotEatenCount = 0;
-struct Fork* forks;
+
 
 int is_finished()
 {
 	int counter = 0;
 	sem_wait(&global_mutex);
 	counter = NotEatenCount;
-	sem_post(&global_mutex);
+	sem_post(&global_mutex);	
 
 	return counter==0;
+
 }
 
+void* philosopher_thread(void *argument)
+{	
+	double priceTableOpen=99.90;
+	double priceTableReorganize=19.90;
+	double rice=20;
+	double price=0;
+	price+=priceTableOpen;
+	price+=(rice*2);
+	struct Philosopher* philosopher = (struct Philosopher*)argument;
+	int again = 1;
+		
+	while(again)
+	{
+		srand(time(NULL));
+		printf("Philosopher %d is Thinking\n", philosopher->number);
+		sleep(rand() % 5 + 1);
+		printf("Philosopher %d is trying to eat...\n", philosopher->number);
+		
+		
+		if (sem_trywait(&forks[philosopher->leftForkIndex].mutex)==0)
+		{
+			int waiting_times = rand() % 5 + 1;
+	
+			while(waiting_times>0){	
+				if (sem_trywait(&forks[philosopher->rightForkIndex].mutex)==0)
+				{		
+					printf("Philosopher %d is Eating\n", philosopher->number);
 
-void philosopher_thread(void *argument){
-    int i;
-    int waitingNumber,status;
-    int groupNum=1;
-    int forkChild[groupNum]; 
 
-    	struct Philosopher* philosopher = (struct Philosopher*)argument;
-    	int again=1;
-    	
-		if((NUMBER_OF_PHILOSOPHERS*NUMBER_OF_TABLE)> NUMBER_OF_PHILOSOPHERS*groupNum){
-			price+=priceTableOpen;
+					if (!philosopher->eatenTimes)
+					{	
+						sem_wait(&global_mutex);
+						NotEatenCount--;
+						sem_post(&global_mutex);
+					}
+
+					philosopher->eatenTimes++;
+
+				//	sleep(rand() % 5 + 1);
+					
+					sem_post(&forks[philosopher->rightForkIndex].mutex);
+
+					waiting_times =- waiting_times;
+				} else {
+					waiting_times--;
+					//sleep(rand() % 5 + 1);								
+				}
+			}
+
+			if (waiting_times==0)
+			{
+				printf("Philosopher %d cannot take second fork...\n", philosopher->number);
+				
+			}
+			/* put left fork on table */
+			sem_post(&forks[philosopher->leftForkIndex].mutex);
+		} else {
+			printf("Philosopher %d cannot eat at this moment...\n", philosopher->number);
+			//tekrarla
+			price+=priceTableReorganize;
+			again=1;
 			
-    			for (i = 0; i < 1; i++){
-    				while(again){	
-        				forkChild[i] = fork();
-        				if (forkChild[i] > 0){//işlemler
-        				//	sleep(rand() % 5 + 1);
-        					int j; 
-        					for(j=0; j<8; j++){
-      						 printf("Philosopher %d is Thinking\n", philosopher[j].number);	
-      					//	 sleep(rand() % 5 + 1);
-      						 printf("Philosopher %d is trying to eat...\n", philosopher[j].number);
-      						 	if (sem_trywait(&forks[philosopher[j].leftForkIndex].mutex)==0 ){
-      								if (sem_trywait(&forks[philosopher[j].rightForkIndex].mutex)==0)
-								{	 
-									printf("Philosopher %d is Eating\n", philosopher[j].number);
-									if (!philosopher[j].eatenTimes){
-										sem_wait(&global_mutex);
-										NotEatenCount--;
-										sem_post(&global_mutex);
-									}
-									
-									philosopher[j].eatenTimes++;
-									sem_post(&forks[philosopher[j].rightForkIndex].mutex);
-									sem_post(&forks[philosopher[j].leftForkIndex].mutex);
-								} 
-      						 	}
-      						 }     						  
-      						 
-        				}
-        				else{
-            					printf("Çıkış yapıyorum\n");
-            					exit(0);
-        				}
-        
-    				} 
-    			}
-    			again = !is_finished();
-    }else{
-    	printf("fazla yazıldı ");
-    }
-  
+		}
+		again = !is_finished();
+		
+	}
+		printf("Price : %.2f",price);
+	
 }
-int main(int argc, char* argv[]){
-	int groupNum,i;
+
+double priceRice=20;
+double riceTableAmount=2000;
+double eatingRiceQuantity=100;
+
+int main(int argc, char* argv[])
+{
+
+	int groupNum;
+
 	printf("Please write a group number\n");
 	scanf("%d",&groupNum);
 	printf("Group Num :\n",groupNum);
 	
-	int count=groupNum*NUMBER_OF_PHILOSOPHERS;
 	
-	struct Philosopher* philosophers = ((struct Philosopher*) malloc(sizeof(struct Philosopher) * (count)));
-	forks = (struct Fork*)malloc(sizeof(struct Fork) * count);
-	
+	int i,j;	
+
+	struct Philosopher* philosophers = (struct Philosopher*) malloc(sizeof(struct Philosopher) * NUMBER_OF_PHILOSOPHERS);
+
+	forks = (struct Fork*)malloc(sizeof(struct Fork) * NUMBER_OF_PHILOSOPHERS);
+
 	sem_init(&global_mutex,0,1);
+
+
+	NotEatenCount = NUMBER_OF_PHILOSOPHERS;
+	for(j=0; j<groupNum; j++){
+	sleep(5);
+	printf("------ table[%d] -----",j);
+		for(i=0; i<NUMBER_OF_PHILOSOPHERS; i++)
+		{
 	
-	for(i=0; i<count; i++)
-	{
-	       sem_init(&forks[i].mutex,0,1);
-   		philosophers[i].eatenTimes = 0;
+	        sem_init(&forks[i].mutex,0,1);
+
+		philosophers[i].eatenTimes = 0;
 		philosophers[i].number = i + 1;
+
 
 		philosophers[i].leftForkIndex = i;
 
-		if (i+1==count){
+		if (i+1==NUMBER_OF_PHILOSOPHERS){
 			philosophers[i].rightForkIndex = 0;
-		}
-		else{
+		}else{
 			philosophers[i].rightForkIndex = i+1;
 		}
-       }
+		
+	}
+	}
 	
-	for(i=0;i<1;i++){
+    	for(i=0;i<NUMBER_OF_PHILOSOPHERS;i++)
         	pthread_create(&philosophers[i].thread_id, NULL, philosopher_thread, &philosophers[i]);
-        	pthread_join(philosophers[i].thread_id, NULL);
-        }
-         printf("price : %d",price);
-return 0;
+
+
+
+	while(!is_finished())
+		usleep(100);
+
+
+	for(i=0;i<NUMBER_OF_PHILOSOPHERS;i++)
+	     pthread_join(philosophers[i].thread_id, NULL);
+
+
+	printf("\nStatistics:\n");
+	for(i=0;i<NUMBER_OF_PHILOSOPHERS;i++){
+		
+		printf("Philosopher %d eaten for %d times\n", philosophers[i].number, philosophers[i].eatenTimes);
+    }
+    	
+
+	free(forks);
+	free(philosophers);		
+	return 0;	
 }
